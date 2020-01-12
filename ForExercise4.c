e0a5e42599905158d18a4b839497d43fe0ad6c7c
Irvinfang <54611412+Irvinfang@users.noreply.github.com>
2020-01-12 15:00:11 +0900

    Update ForExercise4.c

--- ForExercise4.c
+++ ForExercise4.c
@@ -2,19 +2,19 @@
 void main()
 {       
   int i,n;
-  float s = 0.0;
+  float sum = 0.0;
 	printf("Input number : \n");
   scanf("%d",&n);
 	for (i=1;i<=n;i++)
 	{
 		if (i<n){
-      s=s+1/(float)i; 
+      sum=sum+(1/(float)i); 
       printf("1/%d + ", i);
     }
     if (i == n){
-      s=s+1/(float)i;
+      sum=sum+(1/(float)i);
       printf("1/%d", i);
     }
 	}
-  printf("\nSum of series up to %d term is : %f\n", n, s);
+  printf("\nSum of series up to %d term is : %f\n", n, sum);
 }
